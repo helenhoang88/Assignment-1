77cdfd277f7f5823e9573efbcbbdcaba0621562a
Helen Hoang <helenhoang2468@gmail.com>
2026-02-12 22:24:10 +0000

    first commit :] atomic number declared!!

--- Assignment-1.cpp
+++ Assignment-1.cpp
@@ -8,15 +8,17 @@
 int main()
 {
   // Declare variables here
+  int atomic_number;
 
   // Ask user to enter atomic number
-
+  std::cout << "Enter atomic number: ";
+  std::cin >> atomic_number;
   // Ask user to enter initial and final quantum numbers
 
   // Compute photon energy, Delta E = 13.6*(Z^2)*(1/n_j^2-1/n_i^2) eV
 
   // Output answer
-  std::cout << "Here is my answer:" << "..." << std::endl;
+  std::cout << "Here is my answer: " << atomic_number << std::endl;
 
   return 0;
 }
