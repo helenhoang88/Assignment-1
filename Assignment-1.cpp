1889a6a2f238b3c42f05752f61bfaa48d8666dc3
Helen Hoang <helenhoang2468@gmail.com>
2026-02-13 22:31:05 +0000

    defined all integer input variables

--- Assignment-1.cpp
+++ Assignment-1.cpp
@@ -9,16 +9,23 @@ int main()
 {
   // Declare variables here
   int atomic_number;
+  int initial_quantum_number;
+  int final_quantum_number;
 
   // Ask user to enter atomic number
   std::cout << "Enter atomic number: ";
   std::cin >> atomic_number;
   // Ask user to enter initial and final quantum numbers
-
+  std::cout << "Enter intial quantum number: ";
+  std::cin >> initial_quantum_number;
+  std::cout << "Enter final quantum number: ";
+  std::cin >> final_quantum_number;
   // Compute photon energy, Delta E = 13.6*(Z^2)*(1/n_j^2-1/n_i^2) eV
 
   // Output answer
-  std::cout << "Here is my answer: " << atomic_number << std::endl;
+  std::cout << "Atomic number check!! " << atomic_number << std::endl;
+  std::cout << "Inital quantum number check!! " << initial_quantum_number << std::endl;
+  std::cout << "Final quantum number check!! " << final_quantum_number << std::endl;
 
   return 0;
 }
