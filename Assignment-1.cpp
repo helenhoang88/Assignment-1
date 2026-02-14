6981f7b1dd357fbee8dd1ad389d76e33fac74fbb
Helen Hoang <helenhoang2468@gmail.com>
2026-02-14 01:56:40 +0000

    added some input checks

--- Assignment-1.cpp
+++ Assignment-1.cpp
@@ -20,29 +20,57 @@ int main()
       int atomic_number;
       int initial_quantum_number;
       int final_quantum_number;
+      char unit;
       const double ev_to_joule{1.60217663e-19};
 
       // Ask user to enter atomic number
       std::cout << "Enter atomic number: ";
       std::cin >> atomic_number;
+      while (std::cin.fail())
+      {
+        std::cout << "Invalid input. Please enter an integer for atomic number: ";
+        std::cin.clear();
+        std::cin.ignore();
+        std::cin >> atomic_number;
+      }
       // Ask user to enter initial and final quantum numbers
-      std::cout << "Enter intial quantum number: ";
+      std::cout << "Enter initial quantum number: ";
       std::cin >> initial_quantum_number;
+      while (std::cin.fail())
+      {
+        std::cout << "Invalid input. Please enter an integer for initial quantum number: ";
+        std::cin.clear();
+        std::cin.ignore();
+        std::cin >> initial_quantum_number;
+      }
       std::cout << "Enter final quantum number: ";
       std::cin >> final_quantum_number;
+      while (std::cin.fail())
+      {
+        std::cout << "Invalid input. Please enter an integer for final quantum number: ";
+        std::cin.clear();
+        std::cin.ignore();
+        std::cin >> final_quantum_number;
+      }
       // Compute photon energy, Delta E = 13.6*(Z^2)*(1/n_j^2-1/n_i^2) eV
 
       // Output answer
       std::cout << "The transition energy is: " << std::endl;
 
       // Ask user if they want to do another calculation
-      std::cout << "Do you want to do another calculation? (y/n): ";
-      std::cin >> response;
-      if (response == 'n'){
-        std::cout << "Goodbye!" << std::endl;
+      while (true){
+        std::cout << "Do you want to do another calculation? (y/n): ";
+        std::cin >> response;
+        if (response == 'n' || response == 'y')
+          break;
+        std::cout << "Gimme a y or n!!!" << std::endl;
       }
-      else
+      if (response == 'n'){
+        std::cout << "GUD BAI!!!" << std::endl;
+      }
+      else {
         continue;
+      }    
   }
 
   return 0;
